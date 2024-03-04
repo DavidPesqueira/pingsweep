fbb02cfcfb6510d8d632cd292064c4516102a867
rmasterhacker <davidpesqueira@protonmail.com>
2024-03-04 08:33:10 -0800

    Update sweep.sh
    removed a thingy

--- sweep.sh
+++ sweep.sh
@@ -8,4 +8,4 @@ echo -e "${RED} Please enter Network portion of an IP. Example 10.10.10 ${ENDCOL
 read ip
 echo -e "${RED} You entered $ip ${ENDCOLOR}"
 echo -e "${LGREEN}Scanning $ip.0/24 now ${ENDCOLOR}"
-for i in {1..254} ;do (ping -c 1 $ip.$i | grep --color=always "bytes from" | cut -d " " -f4 | tr -d ':' &) ;done
+for i in {1..254} ;do (ping -c 1 $ip.$i | grep "bytes from" | cut -d " " -f4 | tr -d ':' &) ;done
